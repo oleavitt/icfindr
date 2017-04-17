0d4f0b22605a61acc94dcd8a620411263b2123ab
Oren Leavitt <orenl@odysseyinc.com>
2017-04-17 12:39:29 -0500

    Updates for latest version of Xcode 8/Swift 3

--- ICFindr/ICFTableViewCell.swift
+++ ICFindr/ICFTableViewCell.swift
@@ -14,10 +14,10 @@ class ICFTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         
-        self.accessoryType = .DisclosureIndicator
+        self.accessoryType = .disclosureIndicator
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
+    override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
